3a97704901f09f970628c66e4181a6f51b094448
adsussma <adamsussman95@gmail.com>
2016-06-20 17:29:01 -0400

    Add files via upload

---
+++ crypto.py
@@ -0,0 +1,70 @@
+#-------------------------------------------------------------------------------
+# Name:        crypto.py
+# Purpose:     encrypting / decrypting
+#
+# Author:      asussman
+#
+# Created:     20/06/2016
+# Copyright:   (c) asussman 2016
+#-------------------------------------------------------------------------------
+
+def Encryption():
+
+    choice = raw_input("WOULD YOU LIKE TO:" +
+                        '\n' + "(E)NCRYPT" +
+                        '\n' + "(D)ECRYPT" +
+                        '\n' + "(Q)uit")
+    i = 0
+    newString = ''
+    working = True
+
+    while(working):
+
+        encryptList = "LAKSJDHFGMZNXBCVPQOWIEURYT "
+        decryptList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+################################################################################
+        if choice.upper() == "E":
+            toEncrypt = raw_input("PHRASE TO ENCRYPT: ")
+            upEncrypt = toEncrypt.upper()
+                                                                                #MAKE INPUT UPPER TO AVOID ERRORS
+            if upEncrypt.isalpha() :
+                while i < len(upEncrypt):                                       #WHILE THERE ARE LETTERS TO ENCRYPT,
+                    eachLetter = upEncrypt[i]                                   #FOR EACH LETTER IN INPUT FIND ITS
+                    b = decryptList.index(eachLetter)                           #POSITION IN 'decryptList' AND REPLACE
+                    char = encryptList[b]                                       #IT WITH THE LETTER IN 'encryptList'
+                    newString = newString + char                                #WITH THE SAME INDEX. THEN ITERATE TO THE
+                    i = i + 1                                                   #NEXT LETTER
+                placeholder = raw_input("ENCRYPTED PHRASE: %s"  %newString)
+                Encryption()
+            else:
+                pass
+################################################################################
+        if choice.upper() == "D":
+            toDecrypt = raw_input("PHRASE TO DECRYPT: ")
+            upDecrypt = toDecrypt.upper()
+
+            if upDecrypt.isalpha():
+                while i < len(upDecrypt):
+                    eachLetter = upDecrypt[i]
+                    a = encryptList.index(eachLetter)
+                    char = decryptList[a]
+                    newString = newString + char
+                    i = i + 1
+                placeholder = raw_input("DECRYPTED PHRASE: %s"  %newString)
+                Encryption()
+            else:
+                pass
+################################################################################
+        if choice.upper() == "Q":
+            print "THANKS FOR USING CRYPTO.PY"
+            working = False
+            exit()
+################################################################################
+        else:
+            placeholder = raw_input("NOT A VALID RESPONSE, TRY AGAIN")          #PREVENT USER FROM ERRORS
+            Encryption()
+################################################################################
+if __name__ == "__main__":
+    Encryption()
+                                                                                #INITIALIZE FUNCTION
+
